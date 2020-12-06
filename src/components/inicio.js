302b15cedc5057ebf264a2db36ccb24d21d32a8c
Diego Fernando Forero Cruz <df.foreroc@uniandes.edu.co>
2020-12-06 15:15:55 -0500

    code is in react components

--- src/components/inicio.js
+++ src/components/inicio.js
@@ -1,29 +1,58 @@
 import React, { Component } from "react";
-import {ReactComponent as Bell} from "../../public/images/"
-import {ReactComponent as Schedule} from "../images/schedule"
- 
+import '../css/styles.css'
+import Group1 from './img/group-1.png';
+import Group2 from './img/group-2.png';
+import HeaderImg from './img/pageHeader.png';
+
 class Inicio extends Component {
   render() {
     return (
         <div>
-            <header className="jumbotron">
-                <div id="encabezado" className="mx-auto">
-                    <h1 className="display-3 font-weight-bold text-center">Recopilación de Calendarios Académicos</h1>
+            
+            <div className="banner" id="header-section">
+                <div className="container">
+                    <h1 className="font-weight-semibold">Recopilación de calendarios académicos</h1>
+                    <h6 className="font-weight-normal text-muted pb-3">Proyecto de investigación y creación en pregrado</h6>
+                    <img src={HeaderImg} alt="Personas hablando" className="img-fluid vert-move" width="430" />
+                    <br/>
                 </div>
-            </header>
-            <main className="mx-auto">
-                <h2 className="text-center">¡Ten en un solo lugar todas las fechas de los calendarios académicos de Bogotá!</h2>
-                <div className="row mx-auto">
-                    <div className="col-12 col-md-3 offset-md-3">
-                        <Bell />
-                        <p className="font-weight-bold text-center">Obten en un solo lugar las fechas limites de admisión y de eventos de las universidades en Bogotá</p>
-                    </div>
-                    <div className="col-12 col-md-3">
-                        <Schedule />
-                        <p className="font-weight-bold text-center">Recibe notificaciones de las fechas limites de admisión de las universidades de Bogotá</p>
-                    </div>
+            </div>
+            <div className="content-wrapper">
+                <div className="container">   
+                    <section className="features-overview" id="product">
+                        <div className="content-header">
+                            <h2>¿Cómo funciona?</h2>
+                            <h6 className="section-subtitle text-muted">¡Ten en un solo lugar las fechas de los calendarios académicos de Bogotá!</h6>
+                        </div>
+                        <div className="row align-items-center">
+                            <div className="col-12 col-lg-7 grid-margin grid-margin-lg-0" data-aos="fade-right">
+                                <h3 className="m-0" style={{"color": "#DB334C"}}>Calendario</h3>
+                                <div className="col-lg-7 col-xl-6 p-0">
+                                    <p className="py-4 m-0 text-muted">Sabemos que buscar las fechas de todas las universidades a las que piensas postularte puede ser tedioso. </p>
+                                    <p className="font-weight-medium text-muted">Es por eso por lo que creamos un solo lugar donde puedes obtener las fechas límite de admisión (aplicar, becas y eventos importantes) de las mejores universidades en Bogotá.</p>
+                                </div>    
+                            </div>
+                            <div className="col-12 col-lg-5 p-0 img-digital grid-margin grid-margin-lg-0" data-aos="fade-left">
+                                <img src={Group1} alt="Girl with a calendar" className="img-fluid vert-move"/>
+                            </div>
+                        </div>
+                        <div className="row align-items-center">
+                            <div className="col-12 col-lg-7 text-center flex-item grid-margin" data-aos="fade-right">
+                                <img src={Group2} alt="Boy with a mobile phone" className="img-fluid vert-move" />
+                            </div>
+                            <div className="col-12 col-lg-5 flex-item grid-margin" data-aos="fade-left">
+                                <h3 className="m-0" style={{"color": "#EF7F39"}}>Notificaciones</h3>
+                                <div className="col-lg-9 col-xl-8 p-0">
+                                    <p className="py-4 m-0 text-muted">Una vez decidas que universidad y carrera deseas estudiar, es importante no perder las fechas de admisiones. Es por esto que diseñamos una forma fácil y efectiva de comunicarte cuando están próximas a cerrarse las convocatorias seleccionadas.</p>
+                                    <p className="pb-2 font-weight-medium text-muted">Recibe notificaciones de las fechas límite de admisión de las universidades de Bogotá.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </section>     
+                     
                 </div>
-            </main>
+            </div> 
+            
         </div>
     );
   }
--- src/mainPage.js
+++ src/mainPage.js
@@ -3,47 +3,45 @@ import { Route, NavLink, HashRouter } from "react-router-dom";
 
 import Inicio from "./components/inicio";
 import Propuesta from "./components/propuesta";
-import Testimonios from "./components/testimonios";
-import Faq from "./components/faq";
-import Equipo from "./components/equipo";
- 
+import LogoImg from './components/img/logo.png'
+import './css/styles.css'
+
 class MainPage extends Component {
     render() {
         return (
             
             <HashRouter>
-                <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                    <a className="navbar-brand" href="#">Navbar</a>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav">
-                            <li className="nav-item">
-                                <NavLink className="nav-link" to="/" >Inicio</NavLink>
-                            </li>
-                            <li className="nav-item">
-                                <NavLink className="nav-link" to="/propuesta">Propuesta</NavLink>
-                            </li>
-                            <li className="nav-item">
-                                <NavLink className="nav-link" to="/testimonios">Testimonios</NavLink>
-                            </li>
-                            <li className="nav-item">
-                                <NavLink className="nav-link" to="/faq">FAQ</NavLink>
-                            </li>
-                            <li claclassNamess="nav-item">
-                                <NavLink className="nav-link" to="/equipo">Equipo</NavLink>
-                            </li>
-                        </ul>
-                    </div>
-                </nav>                
+                <nav className="navbar navbar-expand-lg pl-3 pl-sm-0" id="navbar">
+                    <div className="container">
+                        <div className="navbar-brand-wrapper d-flex w-100">
+                            <img src={LogoImg} alt="logo" height="70"/>
+                            <button className="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                                <span className="mdi mdi-menu navbar-toggler-icon"></span>
+                            </button> 
+                        </div>
+                        <div className="collapse navbar-collapse navbar-menu-wrapper" id="navbarSupportedContent">
+                            <ul className="navbar-nav align-items-lg-center align-items-start ml-auto">
+                                <li className="d-flex align-items-center justify-content-between pl-4 pl-lg-0">
+                                    <button className="navbar-toggler close-button" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                                        <span className="mdi mdi-close navbar-toggler-icon pl-5"></span>
+                                    </button>
+                                </li>
+                                <li className="nav-item">
+                                    <NavLink className="nav-link" to="/" >Inicio</NavLink>
+                                </li>
+                                <li className="nav-item">
+                                    <NavLink className="nav-link" to="/propuesta">Propuesta</NavLink>
+                                </li>
+                               
+                            </ul>
+                        </div>
+                    </div> 
+                </nav>                 
                 <div>
                     <div className="content">
                         <Route exact path="/" component={Inicio}/>
                         <Route path="/propuesta" component={Propuesta}/>
-                        <Route path="/testimonios" component={Testimonios}/>
-                        <Route path="/faq" component={Faq}/>
-                        <Route path="/equipo" component={Equipo}/>
+                        
                     </div>
                 </div>
             </HashRouter>
